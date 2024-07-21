b6c8c66b8dfa9b5f5b7c885b675db81310a20db2
J-non <whdtjr1513@naver.com>
2024-07-21 15:25:16 +0900

    [feat] : post entity 완료 커밋

--- src/upload/upload.module.ts
+++ src/upload/upload.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UploadService } from './upload.service';
 import { UploadController } from './upload.controller';
+import { MulterModule } from '@nestjs/platform-express';
 
 @Module({
   imports: [
