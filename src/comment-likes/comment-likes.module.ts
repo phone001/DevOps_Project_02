96d4bc5d9c1829228102afd7f7ae3d7884ad0cf4
J-non <whdtjr1513@naver.com>
2024-07-22 15:09:28 +0900

    [fix]: sequelize 의존성 주입, [feat] : post 테이블 테스트 로직

--- src/comment-likes/comment-likes.module.ts
+++ src/comment-likes/comment-likes.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { CommentLikesService } from './comment-likes.service';
 import { CommentLikesController } from './comment-likes.controller';
 import { CreateCommentLikes } from './dto/commentLikes.dto';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { CommentLikes } from './entities/commentLikes.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([CommentLikes])],
   controllers: [CommentLikesController],
   providers: [CommentLikesService, CreateCommentLikes],
 })
--- src/reply/reply.module.ts
+++ src/reply/reply.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReplyService } from './reply.service';
 import { ReplyController } from './reply.controller';
 import { CreateReply } from './dto/reply.dto';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Reply } from './entities/reply.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Reply])],
   controllers: [ReplyController],
   providers: [ReplyService, CreateReply],
 })
