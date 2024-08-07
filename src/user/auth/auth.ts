6c4863a364cec73f81b8dfec232f1443cc2d59fc
gyeon <https://github.com/phone001>
2024-08-07 14:59:16 +0900

    test

--- src/user/auth/auth.ts
+++ src/user/auth/auth.ts
@@ -2,7 +2,7 @@ export class Auth {
     static getToken(req: any): string | undefined {
         console.log(req.cookies.token);
         const token = req.cookies.token || req.headers.authorization.replace("Bearer ", "");
-
+        if (token == 'undefined') return undefined;
         return token;
     }
 }
